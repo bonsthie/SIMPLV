db7a56c618082f0e53167f6dd600da565f20cacb
bonsthie <barnabe.bonnet@gmail.com>
2024-09-25 22:15:23 +0200

    omg le poulet:

--- includes/multiarch/type_avx.h
+++ includes/multiarch/type_avx.h
@@ -14,11 +14,11 @@
 
 /* Define 64-bit integer types based on architecture */
 #if __ARCH_WIDTH == 64
-typedef unsigned long int __uint64_t;
-typedef signed long int __int64_t;
+typedef unsigned long int __sp_uint64_t;
+typedef signed long int __sp_int64_t;
 #else
-__extension__ typedef unsigned long long int __uint64_t;
-__extension__ typedef signed long long int __int64_t;
+__extension__ typedef unsigned long long int __sp_uint64_t;
+__extension__ typedef signed long long int __sp_int64_t;
 #endif 
 
 /* Aligned types (default) */
@@ -26,14 +26,14 @@ __extension__ typedef signed long long int __int64_t;
 /* Normal type */
 typedef double __v4d __attribute__((__vector_size__(32), aligned(32)));
 typedef float __v8f __attribute__((__vector_size__(32), aligned(32)));
-typedef __int64_t __v4ll __attribute__((__vector_size__(32), aligned(32)));
+typedef __sp_int64_t __v4ll __attribute__((__vector_size__(32), aligned(32)));
 typedef int __v8i __attribute__((__vector_size__(32), aligned(32)));
 typedef short __v16s __attribute__((__vector_size__(32), aligned(32)));
 typedef char __v32c __attribute__((__vector_size__(32), aligned(32)));
 typedef signed char __v32sc __attribute__((__vector_size__(32), aligned(32)));
 
 /* Unsigned types */
-typedef __uint64_t __v4ull __attribute__((__vector_size__(32), aligned(32)));
+typedef __sp_uint64_t __v4ull __attribute__((__vector_size__(32), aligned(32)));
 typedef unsigned int __v8ui __attribute__((__vector_size__(32), aligned(32)));
 typedef unsigned short __v16us __attribute__((__vector_size__(32), aligned(32)));
 typedef unsigned char __v32uc __attribute__((__vector_size__(32), aligned(32)));
@@ -43,14 +43,14 @@ typedef unsigned char __v32uc __attribute__((__vector_size__(32), aligned(32)));
 /* Normal type */
 typedef double __uv4d __attribute__((__vector_size__(32), aligned(1)));
 typedef float __uv8f __attribute__((__vector_size__(32), aligned(1)));
-typedef __int64_t __uv4ll __attribute__((__vector_size__(32), aligned(1)));
+typedef __sp_int64_t __uv4ll __attribute__((__vector_size__(32), aligned(1)));
 typedef int __uv8i __attribute__((__vector_size__(32), aligned(1)));
 typedef short __uv16s __attribute__((__vector_size__(32), aligned(1)));
 typedef char __uv32c __attribute__((__vector_size__(32), aligned(1)));
 typedef signed char __uv32sc __attribute__((__vector_size__(32), aligned(1)));
 
 /* Unsigned types */
-typedef __uint64_t __uv4ull __attribute__((__vector_size__(32), aligned(1)));
+typedef __sp_uint64_t __uv4ull __attribute__((__vector_size__(32), aligned(1)));
 typedef unsigned int __uv8ui __attribute__((__vector_size__(32), aligned(1)));
 typedef unsigned short __uv16us __attribute__((__vector_size__(32), aligned(1)));
 typedef unsigned char __uv32uc __attribute__((__vector_size__(32), aligned(1)));
--- includes/multiarch/type_sse.h
+++ includes/multiarch/type_sse.h
@@ -14,11 +14,11 @@
 
 /* Define 64-bit integer types based on architecture */
 #if __ARCH_WIDTH == 64
-typedef unsigned long int __uint64_t;
-typedef signed long int __int64_t;
+typedef unsigned long int __sp_uint64_t;
+typedef signed long int __sp_int64_t;
 #else
-__extension__ typedef unsigned long long int __uint64_t;
-__extension__ typedef signed long long int __int64_t;
+__extension__ typedef unsigned long long int __sp_uint64_t;
+__extension__ typedef signed long long int __sp_int64_t;
 #endif 
 
 /* Aligned types (default) */
@@ -26,14 +26,14 @@ __extension__ typedef signed long long int __int64_t;
 /* Normal type */
 typedef double __v2d __attribute__((__vector_size__(16), aligned(16)));
 typedef float __v4f __attribute__((__vector_size__(16), aligned(16)));
-typedef __int64_t __v2ll __attribute__((__vector_size__(16), aligned(16)));
+typedef __sp_int64_t __v2ll __attribute__((__vector_size__(16), aligned(16)));
 typedef int __v4i __attribute__((__vector_size__(16), aligned(16)));
 typedef short __v8s __attribute__((__vector_size__(16), aligned(16)));
 typedef char __v16c __attribute__((__vector_size__(16), aligned(16)));
 typedef signed char __v16sc __attribute__((__vector_size__(16), aligned(16)));
 
 /* Unsigned types */
-typedef __uint64_t __v2ull __attribute__((__vector_size__(16), aligned(16)));
+typedef __sp_uint64_t __v2ull __attribute__((__vector_size__(16), aligned(16)));
 typedef unsigned int __v4ui __attribute__((__vector_size__(16), aligned(16)));
 typedef unsigned short __v8us __attribute__((__vector_size__(16), aligned(16)));
 typedef unsigned char __v16uc __attribute__((__vector_size__(16), aligned(16)));
@@ -43,14 +43,14 @@ typedef unsigned char __v16uc __attribute__((__vector_size__(16), aligned(16)));
 /* Normal type */
 typedef double __uv2d __attribute__((__vector_size__(16), aligned(1)));
 typedef float __uv4f __attribute__((__vector_size__(16), aligned(1)));
-typedef __int64_t __uv2ll __attribute__((__vector_size__(16), aligned(1)));
+typedef __sp_int64_t __uv2ll __attribute__((__vector_size__(16), aligned(1)));
 typedef int __uv4i __attribute__((__vector_size__(16), aligned(1)));
 typedef short __uv8s __attribute__((__vector_size__(16), aligned(1)));
 typedef char __uv16c __attribute__((__vector_size__(16), aligned(1)));
 typedef signed char __uv16sc __attribute__((__vector_size__(16), aligned(1)));
 
 /* Unsigned types */
-typedef __uint64_t __uv2ull __attribute__((__vector_size__(16), aligned(1)));
+typedef __sp_uint64_t __uv2ull __attribute__((__vector_size__(16), aligned(1)));
 typedef unsigned int __uv4ui __attribute__((__vector_size__(16), aligned(1)));
 typedef unsigned short __uv8us __attribute__((__vector_size__(16), aligned(1)));
 typedef unsigned char __uv16uc __attribute__((__vector_size__(16), aligned(1)));
